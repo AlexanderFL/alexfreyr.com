3b905a350e76dacfa9c84bce079a986e53fa9ab4
AlexanderFL <alex98freyr@gmail.com>
2023-02-05 20:56:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# alexfreyr.com
+
+Repository that contains the source code for my personal website [alexfreyr.com](https://alexfreyr.com). Developed using [SvelteKit](https://kit.svelte.dev/) and [tailwindcss](https://tailwindcss.com/).
+
+# Setup
+
+The setup is as simple as it can get, using [node v18](https://nodejs.org/de/download/).
+
+Install npm dependenices
+
+```shell
+npm install
+```
+
+Run the development server
+
+```shell
+npm run dev
+```
+
+<hr>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<div class="bg-gradient-to-tr from-slate-800 to-slate-900">
+	<div class="h-screen flex flex-col justify-center text-center text-blue-50">
+		<main>
+			<h2>For any inquiries</h2>
+			<h1 class="font-bold text-3xl">
+				<a href="mailto:alex@alexfreyr.com">
+					<span class="un">alex@alexfreyr.com</span>
+				</a>
+			</h1>
+		</main>
+	</div>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		font-family: 'Inter', 'sans-serif';
+	}
+
+	.un {
+		display: inline-block;
+		padding-bottom: 2px;
+		background-image: linear-gradient(theme(colors.slate.50) 0 0);
+		background-position: 0 100%;
+		background-size: 0% 2px;
+		background-repeat: no-repeat;
+		transition: background-size 0.3s, background-position 0s 0.3s;
+	}
+
+	.un:hover {
+		background-position: 100% 100%;
+		background-size: 100% 2px;
+	}
+</style>
