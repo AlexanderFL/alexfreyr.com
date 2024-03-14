2b0bc2762f87894e5ebafa8b803b78f875c2e9be
Alexander Freyr <alex@alexfreyr.com>
2024-03-14 21:11:08 +0000

    Add prettier error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="bg-gray-950 h-screen text-blue-50">
+    <div class="h-screen flex flex-col justify-center text-center">
+        {#if $page.error}
+            <h1 class="text-3xl font-bold tracking-tighter">{$page.status} {$page.error.message}</h1>
+            {#if $page.status === 404}
+                <p class="text-sm">What were you looking for? <a href="/" class="underline font-medium">Go back</a></p>
+            {/if}
+        {/if}
+    </div>
+</div>
