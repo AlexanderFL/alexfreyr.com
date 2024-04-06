233818ecd82d56f8f59346cba2a2e647be2fa468
Alexander Freyr <alex@alexfreyr.com>
2024-04-06 13:43:22 +0000

    Run prettier

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,13 +2,15 @@
 	import { page } from '$app/stores';
 </script>
 
-<div class="bg-gray-950 h-screen text-blue-50">
-    <div class="h-screen flex flex-col justify-center text-center">
-        {#if $page.error}
-            <h1 class="text-3xl font-bold tracking-tighter">{$page.status} {$page.error.message}</h1>
-            {#if $page.status === 404}
-                <p class="text-sm">What were you looking for? <a href="/" class="underline font-medium">Go back</a></p>
-            {/if}
-        {/if}
-    </div>
+<div class="h-screen bg-gray-950 text-blue-50">
+	<div class="flex h-screen flex-col justify-center text-center">
+		{#if $page.error}
+			<h1 class="text-3xl font-bold tracking-tighter">{$page.status} {$page.error.message}</h1>
+			{#if $page.status === 404}
+				<p class="text-sm">
+					What were you looking for? <a href="/" class="font-medium underline">Go back</a>
+				</p>
+			{/if}
+		{/if}
+	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <div class="bg-gradient-to-tr from-slate-800 to-slate-900">
-	<div class="h-screen flex flex-col justify-center text-center text-blue-50">
+	<div class="flex h-screen flex-col justify-center text-center text-blue-50">
 		<main>
 			<h2>For any inquiries</h2>
-			<h1 class="font-bold text-3xl">
+			<h1 class="text-3xl font-bold">
 				<a href="mailto:alex@alexfreyr.com">
 					<span class="un">alex@alexfreyr.com</span>
 				</a>
@@ -11,19 +11,19 @@
 	</div>
 
 	<footer class="fixed bottom-4 w-full">
-		<div class="flex flex-row gap-4 float-right mr-4">
+		<div class="float-right mr-4 flex flex-row gap-4">
 			<a href="https://github.com/AlexanderFL/alexfreyr.com">
 				<img
 					src="./github-mark-white.png"
 					alt="github-logo"
-					class="w-[30px] opacity-70 hover:opacity-100 transition duration-500 ease-in-out"
+					class="w-[30px] opacity-70 transition duration-500 ease-in-out hover:opacity-100"
 				/>
 			</a>
 			<a href="https://stackoverflow.com/users/5258419/alexander-freyr">
 				<img
 					src="./so-logo.png"
 					alt="github-logo"
-					class="w-[30px] opacity-70 hover:opacity-100 transition duration-500 ease-in-out"
+					class="w-[30px] opacity-70 transition duration-500 ease-in-out hover:opacity-100"
 				/>
 			</a>
 		</div>
@@ -42,7 +42,9 @@
 		background-position: 0 100%;
 		background-size: 0% 2px;
 		background-repeat: no-repeat;
-		transition: background-size 0.3s, background-position 0s 0.3s;
+		transition:
+			background-size 0.3s,
+			background-position 0s 0.3s;
 	}
 
 	.un:hover {
